81e6ce6697b34bd881e9eb9f0244b3ba29197aed
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-07-20 14:58:44 +0200

    Embedding Expressions so as to display value dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello world</h1>
+      <div>
+        <span>{this.state.count}</span>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     );
+  }
+
+  formatCount() {
+    const { count: counts } = this.state; // 9 Object Destructuring
+    return counts === 0 ? "zero" : counts;
+    // return counts == 0 ? <h1>zero</h1> : counts;
   }
 }
 
