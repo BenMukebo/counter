a9298d3b43f8209f762d9bc6eaed778e0274c4f1
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-07-22 01:34:44 +0200

    Binding Event Handlers.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,45 +3,42 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: [],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>; // null
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
-    ); //9-Conditional Rendering
-  }
+  handleIncrement = () => {
+    console.log("Increment Clicked", this); // we don't have access to the 'state' property
+  };
+
   render() {
     // let classesName = this.getBadgeClasses();
     return (
       <div>
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */}
-
-        {/* 9-An other technic for Conditional Rendering */}
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </div>
     );
   }
 
-  //   getBadgeClasses() {
-  //     let classesName = "badge m-2 badge-";
-  //     classesName += this.state.count === 0 ? "warning" : "primary";
-  //     return classesName;
-  //   }
+  getBadgeClasses() {
+    let classesName = "badge m-2 badge-";
+    classesName += this.state.count === 0 ? "warning" : "primary";
+    return classesName;
+  }
 
-  //   formatCount() {
-  //     const { count: counts } = this.state; // 9 Object Destructuring
-  //     return counts === 0 ? "zero" : counts;
-  //     // return counts == 0 ? <h1>zero</h1> : counts;
-  //   }
+  formatCount() {
+    const { count: counts } = this.state; // 9 Object Destructuring
+    return counts === 0 ? "zero" : counts;
+    // return counts == 0 ? <h1>zero</h1> : counts;
+  }
 }
 
 export default Counter;
