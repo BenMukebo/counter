f5b99182dfd1c6a17e420e8513272a8e7256c97d
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-08-01 13:15:12 +0200

    Destructuring Arguments of the stateless function component and as well as to properties of props object.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,25 +3,30 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
         <button
           // onClick={() => this.handleReset(this.state.counters)} // (this.props.counter)
           // onClick={() => this.handleReset()} or onClick={this.handleReset}
           // onClick={() => this.props.onReset()}
-          onClick={this.props.onReset}
+          // onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2">
           Reset
         </button>
 
-        {this.props.counters.map((counter) => (
+        {/* {this.props.counters.map((counter) => ( */}
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
             // onDelete={() => this.handleDelete(counter.id)} // 1.1
             // onDelete={() => this.handleDelete(counter)} // 1.2
-            onDelete={this.props.onDelete} // 2
-            onIncrement={this.props.onIncrement} //0000000
+            // onDelete={this.props.onDelete} // 2
+            onDelete={onDelete}
             // onIncrement={() => this.props.onIncrement(counter)} //99999999
+            // onIncrement={this.props.onIncrement} //0000000
+            onIncrement={onIncrement} //0000000
             counter={counter}>
             {/* <h4>Counter # {counter.id}</h4> */}
           </Counter>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
-const Navbar = (props) => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar
         <span className="badge badge-pill badge-secondary m-2">
-          {props.totalCounters}
+          {/* {this.props.totalCounters} */}
+          {/* {props.totalCounters} */}
+          {totalCounters}
         </span>
       </a>
     </nav>
